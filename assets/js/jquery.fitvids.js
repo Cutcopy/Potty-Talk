ec55a8dd69c06a515b60da044d34441bfff3b9ce
Bryce <brycestradling@gmail.com>
2014-09-27 20:31:07 -0500

    Finished colors, nav bar, footer, and partials
    added calander and events. retooled for chris's colors.

--- assets/js/jquery.fitvids.js
+++ assets/js/jquery.fitvids.js
@@ -72,3 +72,14 @@
   };
 // Works with either jQuery or Zepto
 })( window.jQuery || window.Zepto );
+
+$("nav ul li").click(function(){
+  var xcoord = $(this).data("xcoord");
+  
+  $("nav div").stop().animate({marginLeft:xcoord}, 500, "easeInOutExpo");
+  $(this).addClass("active");
+  $("nav ul li").not(this).removeClass("active");
+  
+});
+
+
